de846ee953fb6ec05358252df51941c9c38ac4e7
Elchin Aghayev <Elchin@code.local>
2024-04-30 21:38:29 +0400

    project added

---
+++ Pustok/Pustok/Models/PustokDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pustok.Models
+{
+    public class PustokDbContext:DbContext
+    {
+        public PustokDbContext(DbContextOptions<PustokDbContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookImage> BookImages { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+
+    }
+}
---
+++ Pustok/Pustok/Models/Tag.cs
@@ -0,0 +1,7 @@
+﻿namespace Pustok.Models
+{
+    public class Tag:BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
